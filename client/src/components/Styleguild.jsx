2e144039b865a8ab2a44a9a8e94d2817e2ac7a0f
DuVRP <duduprado2018@gmail.com>
2022-01-10 16:44:44 -0300

    StyleGuild

---
+++ client/src/components/Styleguild.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    color: #FFFFFF;
+    display: flex;
+    flex: 3;
+`;
+
+
+const Styleguild = () => {
+    return (
+        <Container>
+          XXXXXX  
+        </Container>
+    )
+};
+
+export default Styleguild;
